1bdea387bf7af18790aa1c5f25a294dcd4009de3
sarahoco014 <soconnor2@outlook.com>
2023-08-29 17:29:51 +0200

    adds delete fuctionality

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,14 +1,24 @@
 const enterButton = document.querySelector("#enter");
 const list = document.querySelector("#list");
 const toDoItem = document.querySelector("#new-todo");
-const deleteButton = document.querySelectorAll(".delete");
 let newToDoItem;
+const dateButton = document.querySelector("#date");
+const currentDate = document.querySelector("#current-date");
+
+const handleDelete = (newListItem) => {
+    list.removeChild(newListItem);
+}
 
 const createAndAppendListItem = (content) => {
   const newListItem = document.createElement("li");
   const newDeleteButton = document.createElement("button");
   newDeleteButton.innerText = "Delete";
   newDeleteButton.classList.add("delete");
+
+  newDeleteButton.addEventListener("click", () => {
+    handleDelete(newListItem);
+  });
+
   newListItem.innerText = content;
   list.appendChild(newListItem);
   newListItem.appendChild(newDeleteButton);
@@ -24,19 +34,29 @@ enterButton.addEventListener("click", (event) => {
   createAndAppendListItem(newToDoItem);
 });
 
-deleteButton.addEventListener("click", (event) => {});
+const deleteButton = document.querySelectorAll(".delete");
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const newToDo = toDoForm.target;
-  console.log(newToDo);
-  const newListItem = document.createElement("li");
-  newListItem.innerText = newToDo;
+// deleteButton.forEach((button) => {
+//     button.addEventListener("click", (event) => {
+//         console.log(event);
+//     })
+// }) 
 
-  const list = document.querySelector("#list");
-  list.appendChild(newListItem);
-};
-const toDoForm = document.querySelector("#todo-form");
-console.log(toDoForm);
+dateButton.addEventListener("click", (event) => {
+    currentDate.textContent = Date();
+})
 
-toDoForm.addEventListener("submit", handleSubmit);
+// const handleSubmit = (event) => {
+//   event.preventDefault();
+//   const newToDo = toDoForm.target;
+//   console.log(newToDo);
+//   const newListItem = document.createElement("li");
+//   newListItem.innerText = newToDo;
+
+//   const list = document.querySelector("#list");
+//   list.appendChild(newListItem);
+// };
+// const toDoForm = document.querySelector("#todo-form");
+// console.log(toDoForm);
+
+// toDoForm.addEventListener("submit", handleSubmit);
