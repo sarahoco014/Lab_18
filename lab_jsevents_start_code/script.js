d910c07934b34785804303a2440d9bdeb4418dd4
nehcobiy <yibo-chen@hotmail.com>
2023-08-29 15:27:04 +0100

    added delete button

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,50 +1,42 @@
 const enterButton = document.querySelector("#enter");
 const list = document.querySelector("#list");
 const toDoItem = document.querySelector("#new-todo");
+const deleteButton = document.querySelectorAll(".delete");
 let newToDoItem;
 
 const createAndAppendListItem = (content) => {
-    const newListItem = document.createElement("li");
-    newListItem.innerText = content;
-    list.appendChild(newListItem);
-}
+  const newListItem = document.createElement("li");
+  const newDeleteButton = document.createElement("button");
+  newDeleteButton.innerText = "Delete";
+  newDeleteButton.classList.add("delete");
+  newListItem.innerText = content;
+  list.appendChild(newListItem);
+  newListItem.appendChild(newDeleteButton);
+};
 
 toDoItem.addEventListener("input", (event) => {
-     newToDoItem = event.target.value;
-     console.log(newToDoItem);
-})
+  newToDoItem = event.target.value;
+  console.log(newToDoItem);
+});
 
-enterButton.addEventListener("click", createAndAppendListItem(newToDoItem));
+enterButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  createAndAppendListItem(newToDoItem);
+});
 
-
-
-// (event) => {
-//     // const newItem = event.target.value;
-//     // console.log(newItem);
-//     createAndAppendListItem(newToDoItem);
-// }) 
-
-
-
-
-
-
-// Adding a todo item on form submission
-
-
+deleteButton.addEventListener("click", (event) => {});
 
 const handleSubmit = (event) => {
-    event.preventDefault();
-    const newToDo = toDoForm.target;
-    console.log(newToDo);
-    const newListItem = document.createElement('li');
-    newListItem.innerText = newToDo;
+  event.preventDefault();
+  const newToDo = toDoForm.target;
+  console.log(newToDo);
+  const newListItem = document.createElement("li");
+  newListItem.innerText = newToDo;
 
-    const list = document.querySelector('#list');
-    list.appendChild(newListItem);
-
-}
-const toDoForm = document.querySelector('#todo-form');
+  const list = document.querySelector("#list");
+  list.appendChild(newListItem);
+};
+const toDoForm = document.querySelector("#todo-form");
 console.log(toDoForm);
 
-toDoForm.addEventListener('submit', handleSubmit);
+toDoForm.addEventListener("submit", handleSubmit);
