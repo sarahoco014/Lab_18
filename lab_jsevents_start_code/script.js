45fe181eaf336fda406b4fffcf02c0eb2300dc6e
sarahoco014 <soconnor2@outlook.com>
2023-08-29 15:57:08 +0200

    adds event listener for input

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,3 +1,33 @@
+const enterButton = document.querySelector("#enter");
+const list = document.querySelector("#list");
+const toDoItem = document.querySelector("#new-todo");
+let newToDoItem;
+
+const createAndAppendListItem = (content) => {
+    const newListItem = document.createElement("li");
+    newListItem.innerText = content;
+    list.appendChild(newListItem);
+}
+
+toDoItem.addEventListener("input", (event) => {
+     newToDoItem = event.target.value;
+     console.log(newToDoItem);
+})
+
+enterButton.addEventListener("click", createAndAppendListItem(newToDoItem));
+
+
+
+// (event) => {
+//     // const newItem = event.target.value;
+//     // console.log(newItem);
+//     createAndAppendListItem(newToDoItem);
+// }) 
+
+
+
+
+
 
 // Adding a todo item on form submission
 
